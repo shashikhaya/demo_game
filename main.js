87864e2109c44b1f7e5bbe91abc6c547e40a2de7
Shashi <shashi.k.parekh@bath.edu>
2021-03-05 17:40:36 +0000

    working MVP

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 /* eslint-disable no-var */
 
-import { read } from 'fs'
-import { rawListeners } from 'npm'
 import { createInterface } from 'readline'
-
-import promptSync from 'prompt-sync';
-const prompt = promptSync({sigint: true});
-
+import promptSync from 'prompt-sync'
+import { exit } from 'process'
+const prompt = promptSync({ sigint: true })
 
 class Room {
   constructor () {
@@ -16,26 +13,25 @@ class Room {
 
   createOptions () {
     const options = [] // empty array called options
-    options.push('1. inspect desk') // adding option 1
-    options.push('2. try door') // adding option 2
-    // options == ['1','2']
-    return options// options ='1. \n 2. \n
+    options.push('1. Inspect desk') // adding option 1
+    options.push('2. Try door') // adding option 2
+    return options
   }
 
   displayOptions () {
     this.refreshOptions()
-    console.log(this.options.join('\n'))
+    console.log(`Here are your options :\n${this.options.join('\n')}`)
   }
 
   refreshOptions () {
-    this.createOptions()
+    this.options = this.createOptions()
   }
 
   handleInput (input) {
     if (input > this.options.length) {
       console.log('error')
     } else {
-      switch (input) {
+      switch (parseInt(input)) {
         case 1:
           this.inspectDesk()
           break
@@ -46,14 +42,14 @@ class Room {
   }
 
   inspectDesk () {
-    state = 'desk'
+    global.state = 'desk'
   }
 
   tryDoor () {
-    if (this.keyFound === true) {
-      doorOpen = true
+    if (global.keyFound === true) {
+      this.doorOpen = true
     } else {
-      console.log('door cannot be opened')
+      console.log('Oops, you havent unlocked the door yet. Keep trying ;)')
     }
   }
 }
@@ -66,25 +62,25 @@ class Desk {
 
   createOptions () {
     const options = [] // empty array called options
-    options.push(`1. ${(this.drawOpen) ? 'close' : 'open'}`) // adding option 1
-    options.push('2. inspect room') // adding option 2
+    options.push(`1. ${this.drawOpen ? 'Close' : 'Open'} drawer`) // adding option 1
+    options.push('2. Inspect room') // adding option 2
     return options// options ='1. \n 2. \n
   } // call refresh options when option 1 selected
 
   refreshOptions () {
-    this.createOptions()
+    this.options = this.createOptions()
   }
 
   displayOptions () {
     this.refreshOptions()
-    console.log(this.options.join('\n'))
+    console.log(`Here are your options :\n${this.options.join('\n')}`)
   }
 
   handleInput (input) {
     if (input > this.options.length) { // check that input is valid
-      console.log('error')
+      console.log('Error, you cant do that! Try again')
     } else { // choose which method to call
-      switch (input) {
+      switch (parseInt(input)) {
         case 1:
           this.toggleDraw()
           break
@@ -94,40 +90,52 @@ class Desk {
     }
   }
 
-  toggleDraw (state) {
-    this.drawOpen = (!(this.drawOpen))
-    keyFound = true
+  toggleDraw () {
+    this.drawOpen = !(this.drawOpen)
+    global.keyFound = true
   }
 
-  inspectRoom (state) {
-    state = 'room'
+  inspectRoom () {
+    global.state = 'room'
   }
 }
 
 function main () {
   // initialise game
-  var objectDict = initialiseObjects()
-  var state = 'room'
+  global.objectDict = initialiseObjects()
+  global.state = 'room'
+  global.inventory = 'Empty'
   var counter = 0
   const INIT_TIME_MS = Date.now() // Could change -> maybe not necessary -> get elapsed time by subtracting date.now() at start and finish
-  var keyFound = false
+  global.keyFound = false
   // game initialised
 
   // enter game loop
   do {
-    objectDict[state].displayOptions() // displaying updated options for the currently inspected object
-    askQuestion(objectDict, state)
-    // readline.question(`What would you like to do? pick between 1 and ${objectDict[state].options.length}` (ans) => {
-    //   // objectDict[state].handleInput(ans)
-    //   console.log(ans)
-    //   readline.close()
-    // })
-    // counter++
-  } while (objectDict.room.doorOpen === false)
+    console.log('')
+    updateStatus()
+    askQuestion()
+    counter++
+  } while (global.objectDict.room.doorOpen === false)
 
   // doorOpen set to true -> game is finsihed -> log stats
   console.log(
-    ` Well done, you have escaped in ${counter} moves and it took you ${getElapsedTime(INIT_TIME_MS)} minutes`)
+    `Well done, you have escaped in ${counter} moves and it took you ${getElapsedTime(INIT_TIME_MS)} minutes`)
+
+  playAgain()
+}
+
+function playAgain() {
+  console.log('')
+  console.log(`Would you like to play again? 'Yes' or 'No'`)
+  const ans = prompt('>  ')
+  if (ans === 'Yes') {
+    return main()
+  } else if (ans === 'No') {
+    return exit()
+  } else {
+    return playAgain()
+  }
 }
 
 function getElapsedTime (initTime) {
@@ -135,11 +143,23 @@ function getElapsedTime (initTime) {
   const eTime = Date.now() - initTime // times in milliseconds at this point
   return (eTime / 60000).toFixed(2)
 }
+function updateStatus() {
+  updateInventory()
+  console.log(`You are now inspecting the ${global.state}`)
+  console.log(`Inventory: ${global.inventory}`)
+  // if global.keyfound {console.log(inventory)}
+  global.objectDict[global.state].displayOptions() // displaying updated options for the currently inspected object
+  
+}
+function updateInventory() {
+  if ((global.keyFound) && !(global.inventory.includes('Key'))) { global.inventory = ['Key'] }
 
-function askQuestion (objects_dict,state) {
-  let ans = prompt(`What would you like to do? pick between 1 and ${objects_dict[state].options.length}`)
-  objects_dict[state].handleInput(ans)
-  console.log(ans)
+}
+
+function askQuestion () {
+  console.log(`What would you like to do next? Pick between 1 and ${global.objectDict[global.state].options.length}`)
+  const ans = prompt('>  ')
+  global.objectDict[global.state].handleInput(ans)
 }
 
 function initialiseObjects () {
