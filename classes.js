eb03312674c73504666317b08265eb4b4486f74f
JosephSimmons <josephsimmons@outlook.com>
2021-03-12 21:01:33 +0000

    initial changes towards OOP made together this afternoon. Made a game class and changed all variables from global to this. Also tried to use Object.assign() but only makes a copy of the properties in each class, they are not linked.

--- classes.js
+++ classes.js
@@ -16,10 +16,130 @@ b. create options, refresh options and display options -> options are dynamic so
 
 */
 
+export class Game {
+  constructor() {    
+    this.startMenu = new StartMenu() // create startMenu Object -> has the rules and handles the user input for the start loop
+    this.main()
+  }
+
+  // main function
+  main () {
+  this.startMenu.startMenuLoop() // start loop -> should probably be in the startMenu object
+  // enter game loop
+  this.gameLoop() // game loop repeats until user has either quit or won
+  this.playAgain() // gives user option to quit or play again -> repeats until correct input has been provided
+}
+
+  initialiseGameParameters () {
+    this.keyLocations = 2 // number of possible locations for the key
+    this.KEY_LOCATION = (Math.floor(Math.random() * Math.floor(this.keyLocations) + 1)) // used to randomise where the key is (either in the safe or in the desk-drawer)
+    this.objectDict = this.initialiseObjects() // global dictionary with all object instances in it (the game applies different keys to transverse through states)
+    this.state = 'room' // initial state (in the room)
+    this.assignObjectDicts()
+    this.inventory = 'Empty' // inventory starts empty (currently it can only have a key in it, but should add more objects as game develops)
+    // move inventory to person class
+    this.moveCount = 0
+    this.INIT_TIME_MS = Date.now() // time gameplay started (used to calculate the elapsed time when the user has escaped)
+    this.keyFound = false // changes whether the door can be unlocked (proper composition would remove these globals)
+    this.safeUnlocked = false // changes whether the safe can be unlocked (isnt actually being used!!??)
+    
+  }
+
+  assignObjectDicts () {
+    Object.assign(this.objectDict.room, this)
+    Object.assign(this.objectDict.desk, this)
+    Object.assign(this.objectDict.wallPic, this)
+    Object.assign(this.objectDict.safe, this)
+    Object.assign(this.objectDict.window, this)
+  }
+
+  gameLoop () { 
+    
+    this.initialiseGameParameters()
+    // game mechanics
+    console.log( 
+  `##################################################### GAME STARTED #####################################################`)
+    do {
+      this.updateState()
+      console.log(this.objectDict.room.state)
+      console.log(this.state)
+      this.updateStatus() // tell user what move they are on, where they are and what they have in the inventory
+      this.askQuestion() // asks for next move and handles answer
+      this.moveCount++ 
+    } while (!this.objectDict.room.doorOpen) // stopping condition (room is open)
+      //need to try and eliminate objectDict, need the game to "have" a room
+    // if game complete print this
+    console.log(
+      `Well done, you have escaped in ${this.moveCount} moves and it took you ${getElapsedTime(this.INIT_TIME_MS)[0]} minutes and ${getElapsedTime(this.INIT_TIME_MS)[1]} seconds.`)
+  }
+  
+  playAgain () { // if user wants to play again -> recall main function else exit. if input is invalid then call playAgain (recursive fn)
+    console.log('')
+    console.log('Would you like to play again? \'Yes\' or \'No\'')
+    const ans = prompt('>  ')
+    if (ans.toLowerCase() === 'yes') {
+      return main.main()
+    } else if (ans.toLowerCase() === 'no') {
+      return exit()
+    } else {
+      return this.playAgain()
+    }
+  }
+  
+  getElapsedTime (initTime) {
+    // calculate elapsed time
+    const eTime = Date.now() - initTime // times in milliseconds at this point
+    const timeInMins = (eTime / 60000) // time in minutes
+    const minutes = Math.floor(timeInMins)
+    const seconds = Math.round((timeInMins - minutes) * 60)
+    return [minutes, seconds]
+  }
+  
+  updateStatus () { // let user know their status after each move
+    this.updateInventory()
+    console.log(`
+  ##################################################### Move Number ${this.moveCount + 1} ####################################################
+   
+  You are now inspecting the ${this.state} 
+   Inventory: ${this.inventory.includes('Key') ? chalk.yellow.bold(this.inventory) : this.inventory}  
+  `)
+    // if global.keyfound {console.log(inventory)}
+    this.objectDict[this.state].displayOptions() // displaying updated options for the currently inspected object
+  }
+  
+  updateInventory () { // if uswer has key update the inventory
+    if ((this.keyFound) && !(this.inventory.includes('Key'))) { this.inventory = ['Key'] } 
+  }
+
+  updateState () {
+    this.state = this.objectDict[this.state].state
+  }
+  
+  askQuestion () { // let user progress through game  (this.objectDict[this.state] makes sure the right options and handling of answer happens)
+    console.log(`
+  What would you like to do next? Pick between 1 and ${this.objectDict[this.state].options.length}`) // question
+    const ans = prompt('>  ') // answer
+    this.objectDict[this.state].handleInput(ans) // handle answer
+  }
+  
+  initialiseObjects () { // create dictionary with objects in game -> composition would remove the need for this?
+    const objects = {} // keys = location : values = objects (e.g room:Room())
+    objects.room = new Room()
+    objects.desk = new Desk()
+    objects.wallPic = new WallPic()
+    objects.safe = new WallSafe()
+    objects.window = new Window()
+    return objects
+  }
+
+}
+
+
 export class Room {
   constructor () {
     this.options = this.createOptions()
     this.doorOpen = false // global stopping condition (when this is set to true -> door is open, thus the user has escaped)
+    
   }
 
   createOptions () {
@@ -59,20 +179,20 @@ export class Room {
   }
 
   inspectWallPic () { 
-    global.state = 'wallPic'
-    global.objectDict[global.state].displayPicture() // displays picture, composition would make this alot better as the room has a wall picture
+    this.state = 'wallPic'
+    this.objectDict[this.state].displayPicture() // displays picture, composition would make this alot better as the room has a wall picture
   }
 
-  inspectDesk () { // change global state -> should be made a general function ?
-    global.state = 'desk'
+  inspectDesk () { // change this state -> should be made a general function ?
+    this.state = 'desk'
   }
 
-  inspectWindow () { // change global state -> should be made a general function ?
-    global.state = 'window'
+  inspectWindow () { // change this state -> should be made a general function ?
+    this.state = 'window'
   }
 
   tryDoor () { // if keyFound then open, otherwise dont allow it
-    if (global.keyFound) {
+    if (this.keyFound) {
       this.doorOpen = true
     } else {
       console.log('Oops, you havent unlocked the door yet. Keep trying ;)')
@@ -84,7 +204,7 @@ export class Desk {
   constructor () {
     this.drawOpen = false
     this.options = this.createOptions()
-    if (global.KEY_LOCATION === 1) {
+    if (this.KEY_LOCATION === 1) {
       this.hasKey = true
     } else {
       this.hasKey = false
@@ -134,12 +254,12 @@ export class Desk {
   toggleDraw () {
     this.drawOpen = !(this.drawOpen)
     if (this.hasKey) {
-      global.keyFound = true
+      this.keyFound = true
     }
   }
 
   inspectPicture () {
-    const pw = global.objectDict.safe.password
+    const pw = this.objectDict.safe.password
     console.log(
 
   `───────▓▓▓▓▓▓▓────────────▒▒▒▒▒▒
@@ -174,7 +294,7 @@ export class Desk {
   }
 
   inspectRoom () {
-    global.state = 'room'
+    this.state = 'room'
   }
 }
 
@@ -182,8 +302,40 @@ export class StartMenu {
   constructor () {
     this.options = this.createOptions()
     this.gameStarted = false
+    this.displayWelcomeMessage() // display welcome message
+
   }
 
+  startMenuLoop () { // user can either display rules, start game or quit
+    do {
+      this.displayOptions()
+      console.log(`What would you like to do next? Pick between 1 and ${this.options.length}`) // question
+      const ans = prompt('>  ') // answer
+      this.handleInput(ans) // handle answer
+    } while (!this.gameStarted) // repeat until game started or user quits
+  }
+
+  displayWelcomeMessage () { // first thing shown when main function called
+    console.log(
+      `
+        sSSs    sSSs    sSSs   .S_SSSs     .S_sSSs      sSSs         .S_sSSs      sSSs_sSSs      sSSs_sSSs     .S_SsS_S.   
+      d%%SP   d%%SP   d%%SP  .SS~SSSSS   .SS~YS%%b    d%%SP        .SS~YS%%b    d%%SP~YS%%b    d%%SP~YS%%b   .SS~S*S~SS.  
+     d%S'    d%S'    d%S'    S%S   SSSS  S%S    S%b  d%S'          S%S    S%b  d%S'      S%b  d%S'      S%b  S%S  Y' S%S  
+     S%S     S%|     S%S     S%S    S%S  S%S    S%S  S%S           S%S    S%S  S%S       S%S  S%S       S%S  S%S     S%S  
+     S&S     S&S     S&S     S%S SSSS%S  S%S    d*S  S&S           S%S    d*S  S&S       S&S  S&S       S&S  S%S     S%S  
+     S&S_Ss  Y&Ss    S&S     S&S  SSS%S  S&S   .S*S  S&S_Ss        S&S   .S*S  S&S       S&S  S&S       S&S  S&S     S&S  
+     S&S~SP   S&&S   S&S     S&S    S&S  S&S_sdSSS   S&S~SP        S&S_sdSSS   S&S       S&S  S&S       S&S  S&S     S&S  
+     S&S        S*S  S&S     S&S    S&S  S&S~YSSY    S&S           S&S~YSY%b   S&S       S&S  S&S       S&S  S&S     S&S  
+     S*b        l*S  S*b     S*S    S&S  S*S         S*b           S*S    S%b  S*b       d*S  S*b       d*S  S*S     S*S  
+     S*S.      .S*P  S*S.    S*S    S*S  S*S         S*S.          S*S    S%S  S*S.     .S*S  S*S.     .S*S  S*S     S*S  
+      SSSbs  sSS*S    SSSbs  S*S    S*S  S*S          SSSbs        S*S    S&S   SSSbs_sdSSS    SSSbs_sdSSS   S*S     S*S  
+       YSSP  YSS'      YSSP  SSS    S*S  S*S           YSSP        S*S    SSS    YSSP~YSSY      YSSP~YSSY    SSS     S*S  
+                                    SP   SP                        SP                                                SP   
+                                    Y    Y                         Y                                                 Y    
+                                                                      Designed and developed by Shashi, Joseph & Danyaal                                                                                                                                                                                                                                 
+  `)
+  }
+  
   createOptions () {
     const options = [] // empty array called options
     options.push('1. See the rules')
@@ -274,11 +426,11 @@ export class WallPic {
   }
 
   inspectSafe () {
-    global.state = 'safe'
+    this.state = 'safe'
   }
 
   inspectRoom () {
-    global.state = 'room'
+    this.state = 'room'
   }
 
   guessWho () {
@@ -358,7 +510,7 @@ export class WallSafe {
     this.guesses = 0 // counter for attemped guesses of person in picture
     this.hint = 'Hint : Look in the picture on the desk'
     this.doorOpen = false
-    if (global.KEY_LOCATION === 2) {
+    if (this.KEY_LOCATION === 2) {
       this.hasKey = true
     } else {
       this.hasKey = false
@@ -402,7 +554,7 @@ export class WallSafe {
   }
 
   inspectWallPic () {
-    global.state = 'wallPic'
+    this.state = 'wallPic'
   }
 
   toggleDoor () {
@@ -423,9 +575,9 @@ ${this.guesses >= 3 ? chalk.yellow.bold(this.hint) : `${3 - this.guesses} guesse
         this.unlocked = true
         console.log('You are correct! The safe is now unlocked')
         if (this.hasKey) {
-          global.keyFound = true
+          this.keyFound = true
         }
-        global.safeUnlocked = true
+        this.safeUnlocked = true
       } else if (ans.toLowerCase() === 'quit') { break } else {
         console.log('Thats not right, try again!')
         this.guesses++
@@ -508,6 +660,6 @@ export class Window {
   }
 
   inspectRoom () {
-    global.state = 'room'
+    this.state = 'room'
   }
 }
